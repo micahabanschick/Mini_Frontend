1900314dbd8aa73c5706c6ab027d96dc56bfb5a1
micahabanschick <micha.ban@gmail.com>
2021-07-13 22:04:28 -0400

    remove thunk features for the time being

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware, combineReducers, compose} from 'redux'
-import thunk from 'redux-thunk'
+import {createStore, combineReducers, /* applyMiddleware, compose */} from 'redux'
+// import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import {BrowserRouter as Router} from 'react-router-dom'
+import usersReducer from './reducers/usersReducer'
 
 import App from './App';
 
 const rootReducer = combineReducers({
+  user: usersReducer,
+  // project: projectsReducer,
+  // work: worksReducer,
 })
 
 const store = createStore(
   rootReducer, 
   // initialState,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  // compose(
+  //   applyMiddleware(thunk),
+  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
 )
 
 ReactDOM.render(
