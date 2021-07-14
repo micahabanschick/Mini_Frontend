2124f652feb37ac198a9799779ec9f4caef78d60
micahabanschick <micha.ban@gmail.com>
2021-07-13 22:58:16 -0400

    added semantic html to test out color palette

--- src/App.js
+++ src/App.js
@@ -7,8 +7,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <header className="App-header">
+          <h1>sdfsdfsdfsfsd</h1>
+        </header>
+        <section className="App-light-section">
+          <h1>sdfsdfsdfsfsd</h1>
+        </section>
+        <section className="App-dark-section">
+          <h1>sdfsdfsdfsfsd</h1>
+        </section>
+        <section className="App-light-section">
+          <h1>sdfsdfsdfsfsd</h1>
+        </section>
         <h1>Inside App</h1>
         <h1>Outside and After UsersContainer</h1>
+        <footer className="App-footer">
+          <h1>sdfafdsaf</h1>
+        </footer>
       </div>
     );
   }
